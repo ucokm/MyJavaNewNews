3d2919f34795b11c078dd3169a72dbc4db9f0877
ucokm <cox_inside@yahoo.com>
2019-12-15 12:04:43 +0700

    input Country & Category from user

--- app/src/main/java/com/ucokm/myjavanewnews/MainActivity.java
+++ app/src/main/java/com/ucokm/myjavanewnews/MainActivity.java
@@ -1,10 +1,12 @@
 package com.ucokm.myjavanewnews;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.ucokm.myjavanewnews.Adapters.ArticleAdapter;
@@ -12,6 +14,7 @@ import com.ucokm.myjavanewnews.DataModel.Article;
 import com.ucokm.myjavanewnews.DataModel.RespNewsModel;
 import com.ucokm.myjavanewnews.Network.ApiClient;
 import com.ucokm.myjavanewnews.Network.ApiInterface;
+import com.ucokm.myjavanewnews.Utils.CommonUtil;
 import com.ucokm.myjavanewnews.Utils.OnRecyclerViewItemClickListener;
 
 import java.util.List;
@@ -20,24 +23,70 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity implements OnRecyclerViewItemClickListener {
-
-    public static final String API_KEY = "f396737da3054126bfe8d95f230a6b42";
+public class MainActivity extends BaseActivity implements OnRecyclerViewItemClickListener {
+    private RecyclerView rcv_main;
+    private Spinner spnCountry, spnCategory;
+    private Button btnSubmit;
+    private String country = "";
+    private String category = "";
+    final ApiInterface service = ApiClient.getClient().create(ApiInterface.class);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final RecyclerView rcv_main = findViewById(R.id.activity_main_rv);
-        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        rcv_main.setLayoutManager(linearLayoutManager);
+        initViews();
+    }
 
-        final ApiInterface service = ApiClient.getClient().create(ApiInterface.class);
-        Call<RespNewsModel> call = service.getNewsCategories("business", "id", API_KEY);
+    private void initViews() {
+        rcv_main = findViewById(R.id.activity_main_rv);
+        rcv_main.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        spnCountry = findViewById(R.id.spinner_country);
+        spnCountry.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selected = parent.getSelectedItem().toString();
+                country = CommonUtil.Mapper.NewsCountry(selected);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spnCategory = findViewById(R.id.spinner_category);
+        spnCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selected = parent.getSelectedItem().toString();
+                category = CommonUtil.Mapper.NewsCategory(selected);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        btnSubmit = findViewById(R.id.btn_load_news);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doLoadNews();
+            }
+        });
+    }
+
+    private void doLoadNews() {
+        showProgressDialog(null, getString(R.string.progress_loading), false);
+        Call<RespNewsModel> call = service.getNewsCategories(category, country, API_KEY);
         call.enqueue(new Callback<RespNewsModel>() {
             @Override
             public void onResponse(Call<RespNewsModel> call, Response<RespNewsModel> response) {
+                dismissProgressDialog();
                 // check response if status is ok
                 if(response.body().getStatus().equals("ok")) {
                     List<Article> articles = response.body().getArticles();
@@ -52,7 +101,7 @@ public class MainActivity extends AppCompatActivity implements OnRecyclerViewIte
 
             @Override
             public void onFailure(Call<RespNewsModel> call, Throwable t) {
-
+                dismissProgressDialog();
             }
         });
     }
---
+++ app/src/main/java/com/ucokm/myjavanewnews/Utils/CommonUtil.java
@@ -0,0 +1,43 @@
+package com.ucokm.myjavanewnews.Utils;
+
+public class CommonUtil {
+    public static class Mapper {
+        public static String NewsCountry(final String countryName) {
+            switch (countryName) {
+                case "Indonesia":
+                    return "id";
+                case "United States":
+                    return "us";
+                case "France":
+                    return "fr";
+                case "Italy":
+                    return "it";
+                case "Singapore":
+                    return "id";
+                case "New Zealand":
+                    return "nz";
+                case "India":
+                    return "in";
+                default: return "all";
+            }
+        }
+
+        public static String NewsCategory(String categoryName) {
+            switch (categoryName) {
+                case "Business":
+                    return "business";
+                case "Entertainment":
+                    return "entertainment";
+                case "Health":
+                    return "health";
+                case "Science":
+                    return "science";
+                case "Sports":
+                    return "sports";
+                case "Technology":
+                    return "technology";
+                default: return "general";
+            }
+        }
+    }
+}
